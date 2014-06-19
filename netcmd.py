1e3dacfb0c6c3e821ce4c4eb1c90bddbba79b641
Ryan Plyler <g.r.plyler@gmail.com>
2014-06-19 15:38:04 +0800

    Major changes

--- netcmd.py
+++ netcmd.py
@@ -7,6 +7,8 @@ import importlib
 import sys
 import binascii
 from time import sleep
+import select
+import asyncore
 
 # Global settings
 # todo: Write a command and control client for operation pdisk and server nodes to go on the two machines
@@ -24,8 +26,10 @@ class Receiver(threading.Thread):
         self.config = config
         self.stopped = False
         # Need to convert string '\r\n' to literal
-        #self.TERM = self.config['global']['message_terminator']
+        self.TERM = self.config['global']['message_terminator']
         threading.Thread.__init__(self)
+
+        #self.socklist = [self.sock, self.signal_sock]
 
     def run(self):
         # Send connection acknowledgement
@@ -57,29 +61,54 @@ class Receiver(threading.Thread):
 
         # Wait for incoming message (Loop while client is connected)
         # todo: Perform check from control C sent by the client and disconnect
+        # todo: Use select for asychonus reception off the socket
+        # Otherwise the stopped flag doesn't work
         while not self.stopped:
-            data = self.sock.recv(1024)
-            data = data[:-2].decode('UTF-8')
-            print("Received", data, "from", addr)
+            print("looping")
+
+            try:
+                inputready, outputready, exceptready = select.select([self.sock], [], [self.sock])
+            except (OSError):
+                print("Socket Disconnected")
+
+
+            print("Passed select")
+
+            for s in inputready:
+                if s == self.sock:
+                    # Do the reading
+                    data = self.sock.recv(1024)
+                    data = data[:-2].decode('UTF-8')
+                    print("Received", data, "from", addr)
 
 
             # Determine what the message means
             if data in self.config['messages']:
                 self.process_msg(data)
 
+            # If connection is ended by the client
             elif data == "quit":
                 # Shutdown the Connection and Thread
                 print("Disconnecting", addr)
                 self.sendline('Disconnecting...')
-                self.sock.shutdown(0)
-                self.sock.close()
-                self.stopped = True
-                break
+                self.stop()
+
+            # If connection is ended by the server console (Ctrl-C)
+            elif self.stopped:
+                # Stop the connection and thread
+                self.stop()
 
             # Unknown message received
             else:
                 print("Unknown message:", data)
                 self.sendline('Message not understood, see server.yaml')
+
+    def stop(self, console_msg, client_reason):
+        print(console_msg)
+        self.sendline(client_reason)
+        self.sock.shutdown(0)
+        self.sock.close()
+        self.stopped = True
 
     def send_commands(self):
         for i in self.config['messages']:
@@ -105,7 +134,7 @@ class Receiver(threading.Thread):
             self.sendline(response)
 
     def sendline(self, msg):
-        self.sock.send(bytes(msg + TERM, 'utf-8'))
+        self.sock.send(bytes(msg + self.TERM, 'utf-8'))
 
     def get_action(self, action_string):
         mod_name, func_name = action_string.rsplit('.')
@@ -113,37 +142,40 @@ class Receiver(threading.Thread):
         func = getattr(mod, func_name)
         return func
 
+# There asyncore server class
+class Server(asyncore.dispatcher):
+    def __init__(self, name, config):
+        self.config = config
+        self.IP = self.config['global']['bound_ip']
+        self.PORT = self.config['global']['server_port']
+
+        # Stopping flag
+        self.stopped = False
+
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind((IP, PORT))
+        self.listen(10)
+
+    def handle_accepted(self, sock, addr):
+        handler =
+
+    def load_config(self, filename):
+        with open(filename, 'r') as configfile:
+            config = yaml.load(configfile.read())
+
+        return config
 
 if __name__ == "__main__":
     with open('server.yaml') as configfile:
         config = yaml.load(configfile.read())
 
-    # Get Settings
-    IP = config['global']['bound_ip']
-    PORT = config['global']['server_port']
-    stopped = False
-    TERM = config['global']['message_terminator']
+    # Create the server
+    server = Server("netcmd server", config)
 
-    # Create the sockets
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((IP, PORT))
-    server.listen(10)
-    print("netcmd server started on port", PORT)
+    # Start the async loop
+    asyncore.loop()
 
 
 
-    # Create Thread Pool
-    thread_pool = []
-
-    while not stopped:
-        sock, addr = server.accept()
-        print("Accepted connection from", addr)
-        # todo: Authenicate user
-        # Create the receiver thread
-        connection = Receiver(sock, addr, config)
-        connection.start()
-
-        # Wait for single thread to stop
-        #connection.join()
 
